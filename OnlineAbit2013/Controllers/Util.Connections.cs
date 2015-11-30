34bc8a374c7e06ae7254fe9c64ef7296055c5f01
wladengine <wladengine@yandex.ru>
2015-11-30 17:45:20 +0300

    MinorUpd

--- OnlineAbit2013/Controllers/Util.Connections.cs
+++ OnlineAbit2013/Controllers/Util.Connections.cs
@@ -15,11 +15,6 @@ namespace OnlineAbit2013.Controllers
             _abitDB.OpenDatabase(ConstClass.AbitDB);
             _studDB = new SQLClass();
             _studDB.OpenDatabase(ConstClass.StudDB);
-            //_priem2012DB = new SQLClass();
-            //_priem2012DB.OpenDatabase(ConstClass.priem2012DB);
-
-            //_studDB = new SQLClass("Data Source=81.89.183.21;Initial Catalog=EducationUR;Integrated Security=False;User ID=faculty;Password=parolfaculty;MultipleActiveResultSets=True;");
-            //_priem2012DB = new SQLClass("Data Source=srveducation.ad.pu.ru;Initial Catalog=Priem2012;Integrated Security=false;User ID=PriemReader; Password=kukushonok");
         }
     }
 }
